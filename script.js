60e90c6d294d094c6cdc021ad87c394fc5e391ae
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-11-03 13:06:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function mapLetters(word) {
+  const result = {};
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    if (!result[letter]) {
+      result[letter] = [];
+    //  console.log(`Created new array for ${letter}: ${letter}: []`);
+    }
+    result[letter].push(i);
+  //  console.log(`After pushing index ${i}, ${letter}: ${result[letter]}`);
+  }
+
+  return result;
+}
+
+// Example
+console.log(mapLetters("dodo"));
